e9df14f0b0e8ae7e0c26de9b3c171d1f5adb9b53
안호성 <ahnhs2k@anhoseongs-MacBook-Pro.local>
2026-04-13 00:41:46 -0700

    Revert "fix: force-close gallery modal on escape"
    This reverts commit 14389c95145aec2a39379cb53d2930b341a30b5d.

--- src/wwwroot/js/genpage/gentab/currentimagehandler.js
+++ src/wwwroot/js/genpage/gentab/currentimagehandler.js
@@ -19,15 +19,8 @@ class ImageFullViewHelper {
             }
             this.noClose = false;
         }, true);
-        document.addEventListener('keydown', (e) => {
-            if (e.key === 'Escape' && this.isOpen()) {
-                this.close(true);
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        }, true);
         this.modalJq.on('hidden.bs.modal', () => {
-            this.close(false);
+            this.close();
         });
         this.lastMouseX = 0;
         this.lastMouseY = 0;
@@ -349,24 +342,9 @@ class ImageFullViewHelper {
         }
     }
 
-    forceCloseDomState() {
-        this.modal.classList.remove('show');
-        this.modal.style.display = 'none';
-        this.modal.setAttribute('aria-hidden', 'true');
-        document.body.classList.remove('modal-open');
-        document.body.style.removeProperty('padding-right');
-        for (let backdrop of document.querySelectorAll('.modal-backdrop')) {
-            backdrop.remove();
-        }
-    }
-
-    close(force = false) {
-        if (this.isOpen() && !force) {
+    close() {
+        if (this.isOpen()) {
             this.modalJq.modal('hide');
-            this.lastClosed = Date.now();
-        }
-        else {
-            this.forceCloseDomState();
             this.lastClosed = Date.now();
         }
         this.isDragging = false;
